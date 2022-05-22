bc492ee5852230b957164059cf903999b3739cc7
alexakash53 <alexakash53@gmail.com>
2022-05-23 01:08:09 +0600

    tailwind css,daisyUi,google fonts add

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        constructiontheme: {
+          primary: "#FBC22B",
+          secondary: "#FE9013",
+          accent: "#A8171A",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
