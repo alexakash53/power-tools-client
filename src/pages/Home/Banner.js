97897bfe423e7e9a03d984883186afa00b815cd5
alexakash53 <alexakash53@gmail.com>
2022-05-23 13:14:11 +0600

    carosel done

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -2,15 +2,28 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://i.ibb.co/SV1MV6n/Premium-Vector-Construction-tools-supplies-for-home-construction-builder.png" className="max-w rounded-lg" />
-                <div className='pr-20'>
-                    <h1 className="text-5xl font-bold">Your New Smile Starts <br /> Here</h1>
-                    <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
-                    <button className="btn btn-primary text-white uppercase font-blod bg-gradient-to-r from-secondary to-primary">Get Started</button>
-                </div>
+        <div class="carousel w-full">
+            <div id="slide1" class="carousel-item relative w-full">
+                <img src="https://i.ibb.co/ChwcKNW/slider1.png" class="w-full"/> 
+            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide4" class="btn btn-primary bg-inherit">❮</a> 
+                <a href="#slide2" class="btn btn-primary bg-inherit">❯</a>
             </div>
+            </div> 
+            <div id="slide2" class="carousel-item relative w-full">
+                <img src="https://i.ibb.co/4fDytg9/slider3.png" class="w-full"/> 
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide1" class="btn btn-primary bg-inherit">❮</a> 
+                <a href="#slide3" class="btn btn-primary bg-inherit">❯</a>
+                </div>
+            </div> 
+            <div id="slide3" class="carousel-item relative w-full">
+                <img src="https://i.ibb.co/0yPq3x8/slider4.png" class="w-full"/> 
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide1" class="btn btn-primary bg-inherit">❮</a> 
+                <a href="#slide3" class="btn btn-primary bg-inherit">❯</a>
+                </div>
+            </div> 
         </div>
     );
 };
